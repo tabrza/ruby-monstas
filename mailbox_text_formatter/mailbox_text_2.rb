2d330daf1a96459e00ace2c7f748b858a536da5e
Tabish Raza <tabishraza@gmail.com>
2017-10-20 16:58:44 +0100

    Mailbox text formatter can now read from CSV files

---
+++ mailbox_text_formatter/mailbox_text_2.rb
@@ -0,0 +1,133 @@
+class Email
+  def initialize(subject, header)
+    @subject = subject
+    @header = header
+  end
+
+  def subject
+    @subject
+  end
+
+  def date
+    @header[:date]
+  end
+
+  def from
+    @header[:from]
+  end
+end
+
+class Mailbox
+  def initialize(name, emails)
+    @name = name
+    @emails = emails
+  end
+
+  def name
+    @name
+  end
+
+  def emails
+    @emails
+  end
+end
+
+class MailboxTextFormatter
+  def initialize(mailbox)
+    @mailbox = mailbox
+  end
+
+  def format
+    tag(:html, [head, body].join("\n"))
+  end
+
+  def body
+    tag(:body, [headline, table].join("\n"))
+  end
+
+  def headline
+    tag(:h1, @mailbox.name)
+  end
+
+  def table
+    tag(:table, [thead, tbody].join("\n"))
+  end
+
+  def thead
+    tag(:thead, ths.join("\n"))
+  end
+
+  def ths
+    headers = ["Date", "Subject", "From"]
+    headers.map { |content| tag(:th, content) }
+  end
+
+  def tbody
+    tag(:tbody, trs.join("\n"))
+  end
+
+  def trs
+    rows.map { |row| tr(row) }
+  end
+
+  def tr(row)
+    tag(:tr, tds(row).join("\n"))
+  end
+
+  def tds(row)
+    row.map { |content| tag(:td, content) }
+  end
+
+  def tag(name, content)
+    content = "\n#{content}\n" unless [:h1, :td, :th].include?(name)
+    html = "<#{name}>#{content}</#{name}>"
+    html = indent(html) unless name == :html
+    html
+  end
+
+  def indent(html)
+    lines = html.split("\n")
+    lines = lines.map { |line| " " * 2 + line}
+    lines.join("\n")
+  end
+
+  def rows
+    @mailbox.emails.collect do |email|
+      [email.date, email.from, email.subject]
+    end
+  end
+
+  def head
+    "<head>
+    <style>
+      table {
+        border-collapse: collapse;
+      }
+      td, th {
+        border: 1px solid black;
+        padding: 1em;
+      }
+    </style>
+    </head>"
+  end
+end
+
+class EmailCsvStore
+  def initialize(filename)
+    @csv = CSV.new(File.read(filename), headers: true)
+  end
+
+  def read
+    @csv.map do |row|
+      row = row.to_hash
+      Email.new(row['Subject'], { from: row['From'], date: row['Date'] })
+    end
+  end
+end
+
+store = EmailCsvStore.new("emails.csv")
+emails = store.read
+mailbox = Mailbox.new("Ruby Study Group", emails)
+formatter = MailboxTextFormatter.new(mailbox)
+
+File.write("emails.html", formatter.format)
