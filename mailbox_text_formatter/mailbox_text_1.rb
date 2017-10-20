54832a1b36f2a072419cc706d8d9cac38a58a84c
Tabish Raza <tabishraza@gmail.com>
2017-10-20 16:39:31 +0100

    Updated to html format

--- mailbox_text_formatter/mailbox_text_1.rb
+++ mailbox_text_formatter/mailbox_text_1.rb
@@ -18,13 +18,13 @@ class Email
 end
 
 class Mailbox
-  def initialize(subject, emails)
-    @subject = subject
+  def initialize(name, emails)
+    @name = name
     @emails = emails
   end
 
-  def subject
-    @subject
+  def name
+    @name
   end
 
   def emails
@@ -38,41 +38,77 @@ class MailboxTextFormatter
   end
 
   def format
-    lines = [
-      separator,
-      format_row(["Date", "Format", "Subject"]),
-      separator,
-      rows.collect { |row| format_row(row) },
-      separator
-    ]
+    tag(:html, [head, body].join("\n"))
+  end
+
+  def body
+    tag(:body, [headline, table].join("\n"))
+  end
+
+  def headline
+    tag(:h1, @mailbox.name)
+  end
+
+  def table
+    tag(:table, [thead, tbody].join("\n"))
+  end
+
+  def thead
+    tag(:thead, ths.join("\n"))
+  end
+
+  def ths
+    headers = ["Date", "Subject", "From"]
+    headers.map { |content| tag(:th, content) }
+  end
+
+  def tbody
+    tag(:tbody, trs.join("\n"))
+  end
+
+  def trs
+    rows.map { |row| tr(row) }
+  end
+
+  def tr(row)
+    tag(:tr, tds(row).join("\n"))
+  end
+
+  def tds(row)
+    row.map { |content| tag(:td, content) }
+  end
+
+  def tag(name, content)
+    content = "\n#{content}\n" unless [:h1, :td, :th].include?(name)
+    html = "<#{name}>#{content}</#{name}>"
+    html = indent(html) unless name == :html
+    html
+  end
+
+  def indent(html)
+    lines = html.split("\n")
+    lines = lines.map { |line| " " * 2 + line}
     lines.join("\n")
   end
 
-  def separator
-    "+#{columns_widths.map { |width| '-' * (width + 2)}.join("+")}+"
-  end
-
-  def format_row(row)
-    cells = 0.upto(row.length - 1).map do |i|
-      row[i].ljust(columns_widths[i])
-    end
-    "| #{cells.join(" | ")} |"
-  end
-
-  def emails
-    @mailbox.emails
-  end
-
-  def columns_widths
-    columns.map { |column| column.max_by { |cell| cell.length }.length }
-  end
-
-  def columns
-    rows.transpose
-  end
-
   def rows
-    emails.map { |email| [email.date, email.from, email.subject] }
+    @mailbox.emails.collect do |email|
+      [email.date, email.from, email.subject]
+    end
+  end
+
+  def head
+    "<head>
+    <style>
+      table {
+        border-collapse: collapse;
+      }
+      td, th {
+        border: 1px solid black;
+        padding: 1em;
+      }
+    </style>
+    </head>"
   end
 end
 
